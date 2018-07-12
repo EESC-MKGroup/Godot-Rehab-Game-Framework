7025a1927fb1f5270b431af86012d4bb2b9e4c23
Bitiquinho <consoni_2519@hotmail.com>
2018-07-12 17:40:34 -0300

    Refactoring WIP [1]

--- Scripts/info_state_client.gd
+++ Scripts/info_state_client.gd
@@ -4,13 +4,25 @@ enum Request { GET_INFO = 1, DISABLE, ENABLE, RESET, PASSIVATE, OPERATE, OFFSET,
 enum Reply { GOT_INFO = 1, DISABLED, ENABLED, ERROR, PASSIVE, OPERATING, OFFSETTING, CALIBRATING, PREPROCESSING, USER_SET, CONFIG_SET }
 
 signal state_changed
+signal client_connected
 
 var connection = StreamPeerTCP.new()
 
-var info = {}
+var axes_list = []
 
 func _ready():
 	set_process( false )
+
+func _process( delta ):
+	var reply_code = connection.get_u8()
+	if reply_code == Reply.GOT_INFO:
+		var info_string = connection.get_string()
+		var info = parse_json( info_string )
+		axes_list.clear()
+		var robot_name = info[ "id" ]
+		for axis_name in info[ "axes" ]:
+			axes_list.append( robot_name + "-" + axis_name )
+	emit_signal( "state_changed", reply_code )
 
 func connect_client( host, port ):
 	if not connection.is_connected_to_host():
@@ -19,12 +31,12 @@ func connect_client( host, port ):
 			print( "connecting to %s:%d" % [ host, port ] )
 			continue
 		if connection.is_connected_to_host(): 
-			output_status = 1
+			emit_signal( "client_connected" )
 			set_process( true )
 
-func _process( delta ):
-	var reply_code = connection.get_u8()
-	if reply_code == Reply.GOT_INFO:
-		var info_string = connection.get_string()
-		info = parse_json( info_string )
-	emit_signal( "state_changed", reply_code )
+func refresh_axes_info():
+	if connection.is_connected_to_host():
+		connection.put_u8( GET_INFO )
+
+func get_axes_list():
+	return axes_list
--- Scripts/initialization_options.gd
+++ Scripts/initialization_options.gd
@@ -1,45 +1,57 @@
 extends Panel
 
+const INFO_SERVER_STATE_PORT = 50000
+
 onready var position_slider = get_node( "PositionSlider" )
 onready var position_display = get_node( "PositionSlider/NumericDisplay" )
 
 func _ready():
 	$AddressInput.text = Configuration.get_parameter( "server_address" )
 	$UserInput.text = Configuration.get_parameter( "user_name" )
-	$CalibrationToggle.pressed = Controller.is_calibrating
-	$DirectionSelector.pressed = ( Controller.direction_axis == Controller.HORIZONTAL )
-	Controller.set_status( 1 )
+	$CalibrationToggle.pressed = RemoteAxisClient.is_calibrating
+	$DirectionSelector.pressed = ( RemoteAxisClient.direction_axis == RemoteAxisClient.HORIZONTAL )
+	InfoStateClient.connect( "state_changed", self, "_on_state_changed" )
+	InfoStateClient.connect( "client_connected", self, "_on_client_connected" )
 
 func _input( event ):
 	if event is InputEventKey:
 		if event.scancode == KEY_ESCAPE: get_tree().quit()
 
 func _process( delta ):
-	var axis_values = Controller.get_axis_values()
-	var position = axis_values[ Controller.POSITION ]
+	var axis_values = RemoteAxisClient.get_axis_values()
+	var position = axis_values[ RemoteAxisClient.POSITION ]
 	position_slider.value = position
 	position_display.text = ( "%+.3f" % position )
 
 func _on_ConnectButton_pressed():
 	var server_address = Configuration.get_parameter( "server_address" )
-	Controller.connect_client( server_address, 8000 )
-	var user_name = Configuration.get_parameter( "user_name" )
-	var time_stamp = OS.get_unix_time()
-	Controller.set_identifier( user_name, time_stamp )
-	DataLog.create_new_log( user_name, time_stamp )
+	InfoStateClient.connect_client( server_address, INFO_SERVER_STATE_PORT )
+	InfoStateClient.refresh_axes_info()
+#	var user_name = Configuration.get_parameter( "user_name" )
+#	var time_stamp = OS.get_unix_time()
+#	RemoteAxisClient.set_identifier( user_name, time_stamp )
+#	DataLog.create_new_log( user_name, time_stamp )
+
+func _on_state_changed( new_state ):
+	if new_state == InfoStateClient.GOT_INFO:
+		var axes_list = InfoStateClient.get_axes_list()
+
+func _on_client_connected():
+	$ConnectButton.text = "Refresh"
 
 func _on_AddressInput_text_changed( new_text ):
 	Configuration.set_parameter( "server_address", new_text )
+	$ConnectButton.text = "Connect"
 
 func _on_UserInput_text_changed( new_text ):
 	Configuration.set_parameter( "user_name", new_text )
 
 func _on_SetpointSlider_value_changed( value ):
-	Controller.set_axis_values( value, 1 )
+	RemoteAxisClient.set_axis_values( value, 1 )
 
 func _on_CalibrationToggle_toggled( button_pressed ):
-	Controller.is_calibrating = button_pressed
+	RemoteAxisClient.is_calibrating = button_pressed
 
 func _on_DirectionSelector_toggled( button_pressed ):
-	if button_pressed: Controller.direction_axis = Controller.HORIZONTAL
-	else: Controller.direction_axis = Controller.VERTICAL
+	if button_pressed: RemoteAxisClient.direction_axis = RemoteAxisClient.HORIZONTAL
+	else: RemoteAxisClient.direction_axis = RemoteAxisClient.VERTICAL
