fa9ecfe8f6702eb1429fe0f960aef82cf466500d
Bitiquinho <consoni_2519@hotmail.com>
2019-06-19 14:28:41 -0300

    Multi-axis refactoring WIP 2

--- Scripts/initialization_options.gd
+++ Scripts/initialization_options.gd
@@ -1,21 +1,22 @@
 extends Panel
 
-onready var position_slider = get_node( "PositionSlider" )
-onready var position_display = get_node( "PositionSlider/NumericDisplay" )
-
 func _ready():
 	if OS.get_cmdline_args().size() > 1:
-		if OS.get_cmdline_args()[ 0 ] == "-server":
+		if OS.get_cmdline_args()[ 0 ] == "--server":
 			GameConnection.is_server = true
 			GameManager.select( OS.get_cmdline_args()[ 1 ] )
 	$AddressInput.text = Configuration.get_parameter( "server_address" )
 	$UserInput.text = Configuration.get_parameter( "user_name" )
 	$CalibrationToggle.pressed = InputAxis.is_calibrating
-	var font = $InterfaceSelector/SelectionList.get_font( "font" )
-	$InterfaceSelector/SelectionList.get_popup().add_font_override( "font", font )
+	var font = $DeviceSelector/SelectionList.get_font( "font" )
+	$DeviceSelector/SelectionList.get_popup().add_font_override( "font", font )
 	$AxisSelector/SelectionList.get_popup().add_font_override( "font", font )
-	$InterfaceSelector/SelectionList.get_popup().connect( "index_pressed", self, "_on_Device_index_pressed" )
+	$GameSelector/SelectionList.get_popup().add_font_override( "font", font )
+	$VariableSelector/SelectionList.get_popup().add_font_override( "font", font )
+	$DeviceSelector/SelectionList.get_popup().connect( "index_pressed", self, "_on_Device_index_pressed" )
 	$AxisSelector/SelectionList.get_popup().connect( "index_pressed", self, "_on_Axis_index_pressed" )
+	$GameSelector/SelectionList.get_popup().connect( "index_pressed", self, "_on_Game_index_pressed" )
+	$VariableSelectorSelector/SelectionList.get_popup().connect( "index_pressed", self, "_on_Variable_index_pressed" )
 	InputDevice.connect( "state_changed", self, "_on_state_changed" )
 	InputDevice.connect( "socket_connected", self, "_on_socket_connected" )
 	_refresh_devices_list()
@@ -25,19 +26,20 @@ func _input( event ):
 		if event.scancode == KEY_ESCAPE: get_tree().quit()
 
 func _process( delta ):
-	position_slider.value = InputAxis.get_value()
-	position_display.text = ( "%+.3f" % position_slider.value )
+	$PositionSlider.value = InputAxis.get_value()
+	$PositionSlider/NumericDisplay.text = ( "%+.3f" % $PositionSlider.value )
+	$ForceSlider.value = InputAxis.get_value()
+	$ForceSlider/NumericDisplay.text = ( "%+.3f" % $ForceSlider.value )
 
 func _on_ConnectButton_pressed():
 	print( "_on_ConnectButton_pressed" )
 	InputDevice.connect_socket( $AddressInput.text )
-	InputDevice.state = InputDevice.GET_INFO
 #	DataLog.create_new_log( user_name, time_stamp )
 
 func _refresh_devices_list():
-	$InterfaceSelector/SelectionList.get_popup().clear()
+	$DeviceSelector/SelectionList.get_popup().clear()
 	for interface_name in InputDevice.interfaces_list:
-		$InterfaceSelector/SelectionList.get_popup().add_item( interface_name )
+		$DeviceSelector/SelectionList.get_popup().add_item( interface_name )
 	_on_Device_index_pressed( 0 )
 
 func _refresh_axes_list():
@@ -50,7 +52,7 @@ func _refresh_axes_list():
 
 func _on_state_changed( new_state ):
 	match new_state:
-		InputDevice.GET_INFO:
+		InputDevice.LIST_CONFIGS:
 			_refresh_devices_list()
 		InputDevice.OFFSET:
 			$OffsetToggle.pressed = true
@@ -66,8 +68,9 @@ func _on_state_changed( new_state ):
 func _on_Device_index_pressed( index ):
 	print( "_on_Device_index_pressed" )
 	InputDevice.interface_index = index
-	var interface_name = $InterfaceSelector/SelectionList.get_popup().get_item_text( InputDevice.interface_index )
-	$InterfaceSelector/SelectionList.text = interface_name
+	var interface_name = $DeviceSelector/SelectionList.get_popup().get_item_text( InputDevice.interface_index )
+	$DeviceSelector/SelectionList.text = interface_name
+	InputDevice.state = InputDevice.SET_CONFIG
 	_refresh_axes_list()
 
 func _on_Axis_index_pressed( index ):
@@ -80,7 +83,7 @@ func _on_socket_connected():
 	print( "_on_socket_connected" )
 	Configuration.set_parameter( "server_address", $AddressInput.text )
 	Configuration.set_parameter( "user_name", $UserInput.text )
-	_refresh_axes_list()
+	InputDevice.state = InputDevice.LIST_CONFIGS
 
 func _on_AddressInput_text_changed( new_text ):
 	print( "_on_AddressInput_text_changed" )
